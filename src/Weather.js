6fce7b2ffad0e7e36ca0eb2c692233059d306639
KateKlu <ktrn0514@gmail.com>
2025-07-24 23:59:15 +0200

    add .env and modal

--- src/Weather.js
+++ src/Weather.js
@@ -1,20 +1,36 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-
 import WeatherInfo from './WeatherInfo';
 import WeatherForecast from './WeatherForecast';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 import './Weather.css';
 
 export default function Weather(props) {
    const [weatherData, setWeatherData] = useState({ ready: false });
    const [city, setCity] = useState(props.defaultCity);
+   const [isLoading, setIsLoading] = useState(false);
+   const [showModal, setShowModal] = useState(false);
+   const [modalMsg, setModalMsg] = useState('');
+
+   function openModal(msg) {
+      setModalMsg(msg);
+      setShowModal(true);
+   }
+
+   useEffect(() => {
+      search(city);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, []);
 
    function handleResponse(response) {
+      setIsLoading(false);
+
       setWeatherData({
          ready: true,
          city: response.data.name,
          country: response.data.sys.country,
-         data: new Date(response.data.dt * 1000),
+         date: new Date(response.data.dt * 1000),
          temp: response.data.main.temp,
          description: response.data.weather[0].description,
          humidity: response.data.main.humidity,
@@ -24,52 +40,82 @@ export default function Weather(props) {
       });
    }
 
-   function search() {
-      const apiKey = 'ed55b36e362d8733f7d859247cedeaf2';
-      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+   function search(term) {
+      setIsLoading(true);
+      const apiKey = process.env.REACT_APP_WEATHER_KEY;
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${term}&appid=${apiKey}&units=metric`;
 
-      axios.get(apiUrl).then(handleResponse);
+      axios
+         .get(apiUrl)
+         .then(handleResponse)
+         .catch((error) => {
+            setIsLoading(false);
+            if (error.response?.status === 404) {
+               openModal(`City “${term}” not found. Try another.`);
+               setWeatherData({ ready: false });
+            } else {
+               openModal('Network error. Please try again later.');
+            }
+         });
    }
 
    function handleSubmit(event) {
       event.preventDefault();
-      search();
+      if (!city.trim()) {
+         openModal('Please enter a city name.');
+         return;
+      }
+      search(city);
    }
 
    function handleCityChange(event) {
       setCity(event.target.value);
    }
 
-   if (weatherData.ready) {
-      return (
-         <div className="Weather">
-            <form onSubmit={handleSubmit}>
-               <div className="row">
-                  <div className="col-9 SearchInput">
-                     <input
-                        type="search"
-                        placeholder="Enter a city..."
-                        autoFocus="on"
-                        className="w-100 h-100 "
-                        onChange={handleCityChange}
-                     />
-                  </div>
-                  <div className="col-3 SearchBtn">
-                     <input
-                        type="submit"
-                        value="Search"
-                        className="btn btn-primary w-100 SearchBtnText"
-                     />
-                  </div>
+   return (
+      <div className="Weather">
+         <form onSubmit={handleSubmit}>
+            <div className="row">
+               <div className="col-9 SearchInput">
+                  <input
+                     type="search"
+                     placeholder="Enter a city..."
+                     autoFocus="on"
+                     defaultValue={props.defaultCity}
+                     className="w-100 h-100 "
+                     onChange={handleCityChange}
+                  />
                </div>
-            </form>
-            <WeatherInfo data={weatherData} />
-            <WeatherForecast coordinates={weatherData.coordinates} />
-         </div>
-      );
-   } else {
-      search();
+               <div className="col-3 SearchBtn">
+                  <input
+                     type="submit"
+                     value="Search"
+                     className="btn btn-primary w-100 SearchBtnText"
+                  />
+               </div>
+            </div>
+         </form>
 
-      return 'Loading...';
-   }
+         {isLoading && <p>Loading...</p>}
+
+         {!isLoading && weatherData.ready && (
+            <>
+               <WeatherInfo data={weatherData} />
+               <WeatherForecast coordinates={weatherData.coordinates} />
+            </>
+         )}
+
+         <Modal show={showModal} onHide={() => setShowModal(false)} centered>
+            <Modal.Header closeButton>
+               <Modal.Title>Notice</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{modalMsg}</Modal.Body>
+            <Modal.Footer>
+               <Button variant="primary" onClick={() => setShowModal(false)}>
+                  OK
+               </Button>
+            </Modal.Footer>
+         </Modal>
+      </div>
+   );
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,32 +1,32 @@
-import React from "react";
-import FormatDate from "./FormatDate";
-import WeatherIcon from "./WeatherIcon";
-import WeatherTemperature from "./WeatherTemperature";
+import React from 'react';
+import FormatDate from './FormatDate';
+import WeatherIcon from './WeatherIcon';
+import WeatherTemperature from './WeatherTemperature';
 
 export default function WeatherInfo(props) {
-  return (
-    <div className="WeatherInfo">
-      <h1>
-        {props.data.city}, {props.data.country}
-      </h1>
-      <ul>
-        <li>
-          <FormatDate date={props.data.data} />
-        </li>
-        <li className="weatherDescription">{props.data.description}</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-8">
-          <WeatherIcon code={props.data.icon} size={50} />
-          <WeatherTemperature celsius={props.data.temp} />
-        </div>
-        <div className="col-4">
-          <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} m/s</li>
-          </ul>
-        </div>
+   return (
+      <div className="WeatherInfo">
+         <h1>
+            {props.data.city}, {props.data.country}
+         </h1>
+         <ul>
+            <li>
+               <FormatDate date={props.data.date} />
+            </li>
+            <li className="weatherDescription">{props.data.description}</li>
+         </ul>
+         <div className="row mt-3">
+            <div className="col-8">
+               <WeatherIcon code={props.data.icon} size={50} />
+               <WeatherTemperature celsius={props.data.temp} />
+            </div>
+            <div className="col-4">
+               <ul>
+                  <li>Humidity: {props.data.humidity}%</li>
+                  <li>Wind: {props.data.wind} m/s</li>
+               </ul>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
